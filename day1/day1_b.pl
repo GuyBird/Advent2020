e7be05bd170ecbfe21e320a2b41883ffefcaa3d8
Guy Bird <06gbird@gmail.com>
2021-12-01 20:06:55 +0000

    Add day 1 solutions

---
+++ day1/day1_b.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $target = 2020;
+
+my $filename = 'day1_input';
+open(FH, '<', $filename) or die $!;
+
+my @numbers = <FH>;
+close(FH);
+
+
+my $highest_tested = 0;
+for my $test_number (@numbers) {
+    $highest_tested++;
+    for my $second_number_index ($highest_tested .. scalar @numbers - 1) {
+        if ($test_number +  $numbers[$second_number_index] < $target ) {
+            for my $third_number_index ($second_number_index .. scalar @numbers - 1) {
+                if ($test_number + $numbers[$second_number_index] + $numbers[$third_number_index] == $target) {
+                    my $answer = $test_number * $numbers[$second_number_index] * $numbers[$third_number_index];
+                    print $answer; 
+                }
+            }
+        }
+    }
+}
