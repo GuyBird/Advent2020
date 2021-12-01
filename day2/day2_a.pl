d229edd65e46dbd4d5bc1087883286dbee8251e0
Guy Bird <06gbird@gmail.com>
2021-12-01 21:37:46 +0000

    Add solution to day 2 part 1

---
+++ day2/day2_a.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $filename = 'day2_input';
+open(FH, '<', $filename) or die $!;
+
+my @passwords = <FH>;
+close(FH);
+
+my $valid_passwords = 0;
+for (@passwords) {
+    my $current_matches = 0;
+    if ($_ =~ /(\d+)-(\d+)\W(\w):\W(\w+)/) {
+        my $min_matches =  $1;
+        my $max_matches =  $2;
+        my $matches = () = $4 =~ /$3/g;
+        $valid_passwords++ if ($min_matches <= $matches && $matches <= $max_matches);
+    } else {
+        print "[ERROR] password REGEX did not match\n";
+    }
+}
+
+print $valid_passwords;
